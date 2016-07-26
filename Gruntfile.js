0df11cd885c41a201e24ad6eaef27a03aa4ae229
Ivan Reyne <ivan@inneox.com>
2016-07-26 17:05:56 -0500

    Initial structure setup

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        buildDir: 'dist',
+        srcDir: '.',
+        clean: {
+            clean: ['<%= buildDir %>']
+        },
+        // copy: {
+        //     build: {
+        //         // files we need to copy for the "build" version
+        //         files: [
+        //             {expand: true, cwd: '<%= srcDir %>/www', src: ['**', '!style/**'], dest: '<%= buildDir %>/www/'},
+        //             {expand: true, cwd: '<%= srcDir %>/jsLib', src: ['**'], dest: '<%= buildDir %>/jsLib/'}
+        //         ]
+        //     }
+        // },
+        jshint: {
+            build: {
+                files: {
+                    src: ['Gruntfile.js', '<%= srcDir %>/js/**/*.js', '!<%= srcDir %>/test/**/*.js']
+                },
+                options: {
+                    globals: {
+                        jQuery: true
+                    }
+                }
+            }
+        },
+        less: {
+            build: {
+                options: {
+                    compress: true,
+                    optimization: 1
+                },
+                files: {
+                    '<%= buildDir %>/lautrec.css': [
+                        '<%= srcDir %>/less/lautrec.less',
+                        '<%= srcDir %>/bower_components/animate.css/animate.css'
+                    ]
+                }
+            }
+        },
+        mkdir: {
+            build: {
+                options: {
+                    create: [
+                        '<%= buildDir %>'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    '<%= buildDir %>/lautrec.min.js': ['<%= srcDir %>/js/**/*.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mkdir');
+
+    grunt.registerTask('build', ['jshint', 'mkdir', 'less', 'uglify']);
+    grunt.registerTask('run', ['build']);
+};
---
+++ js/lautrec.js
@@ -0,0 +1,4 @@
+
+$(body).on('click', '.test-lautrec-click', function() {
+    console.log('Test Lautrec JS click');
+});
